41a7816686695784b6f7a595f1b53a4a55b2ee70
Anna Kornieieva <109692085+KornieievaAnna@users.noreply.github.com>
2023-02-23 19:44:46 +0200

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,4 @@
-
-export * from 'components/Сounter/Section/Section';
-export * from 'components/Сounter/Feedbacks/Feedbacks';
-export * from 'components/Сounter/Statistics/Statistics';
-export * from 'components/Сounter/Statistics/Notification/Notification';
+export * from './Сounter/Section/Section';
+export * from './Сounter/Feedbacks/Feedbacks';
+export * from './Сounter/Statistics/Statistics';
+export * from './Сounter/Statistics/Notification/Notification';
