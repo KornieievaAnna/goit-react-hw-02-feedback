e24efb2cff542a2e6b92da1a8c009455f91feb1e
Anna Kornieieva <109692085+KornieievaAnna@users.noreply.github.com>
2023-02-23 20:50:33 +0200

    update export App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,106 @@
-import Couter from "./Сounter/Counter";
+// import Couter from "./Сounter/Counter";
 
 
- const App = () => {
-  return (
-    <div
-      style={{
-        // height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        // alignItems: 'center',
-        fontSize: 23,
-        color: '#010101'
-      }}
-    >      
-      <Couter/>
-    </div>
+//  const App = () => {
+//   return (
+//     <div
+//       style={{
+//         // height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         // alignItems: 'center',
+//         fontSize: 23,
+//         color: '#010101'
 
-  );
-};
+// border: '1px solid black',
+//     borderRadius: '5px',
+//     padding: '10px',
+//     background-image: 'linear-gradient(270deg, #33353a 15%, #000 65%)',
+//     color: 'rgb(235, 229, 222)',
 
+//       }}
+//     >
+//       <Couter/>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+import { Feedback, Statistic, Section, Notification } from 'components/index';
+// import css from './Counret.module.css';
+
+// import Statistic from 'components/Сounter/Statistics/Statistics';
+// import Section from 'components/Сounter/Section/Section';
+// import Notification from 'components/Сounter/Statistics/Notification/Notification';
+import React, { Component } from 'react';
+// import css from './Counret.module.css';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  countTotalFeedback() {
+    return this.state.good + this.state.neutral + this.state.bad;
+  }
+
+  countPositiveFeedbackPercentage = () => {
+    if (this.countTotalFeedback) {
+      return Math.round((this.state.good / this.countTotalFeedback()) * 100);
+    }
+    return 0;
+  };
+
+  showFeedback = options => {
+    this.setState(prevState => ({ [options]: prevState[options] + 1 }));
+  };
+
+  // handleIncrementGood = () => {
+  //   this.setState(prevState => ({
+  //     good: prevState.good + 1,
+  //   }));
+  // };
+
+  // handleIncrementNeutral = () => {
+  //   this.setState(prevState => ({
+  //     neutral: prevState.neutral + 1,
+  //   }));
+  // };
+
+  // handleIncrementBad = () => {
+  //   this.setState(prevState => ({
+  //     bad: prevState.bad + 1,
+  //   }));
+  // };
+
+  render() {
+    return (
+      <div style={{}}>
+        <Section title="Please leave feedback">
+          <Feedback
+            options={Object.keys(this.state)}
+            onLeaveFeedback={this.showFeedback}
+          />
+        </Section>
+        <Section title="Statistics">
+          {this.countTotalFeedback() !== 0 ? (
+            <Statistic
+              good={this.state.good}
+              neutral={this.state.neutral}
+              bad={this.state.bad}
+              total={this.countTotalFeedback()}
+              percentage={this.countPositiveFeedbackPercentage()}
+            />
+          ) : (
+            <Notification message="There is no feedback"></Notification>
+          )}
+        </Section>
+      </div>
+    );
+  }
+}
 export default App;
+
--- src/components/Сounter/Counter.js
+++ src/components/Сounter/Counter.js
@@ -1,76 +1,76 @@
-import { Feedback, Statistic, Section, Notification } from 'components/index';
-import css from './Counret.module.css';
-
-// import Statistic from 'components/Сounter/Statistics/Statistics';
-// import Section from 'components/Сounter/Section/Section';
-// import Notification from 'components/Сounter/Statistics/Notification/Notification';
-import React, { Component } from 'react';
+// import { Feedback, Statistic, Section, Notification } from 'components/index';
 // import css from './Counret.module.css';
 
-class Couter extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+// // import Statistic from 'components/Сounter/Statistics/Statistics';
+// // import Section from 'components/Сounter/Section/Section';
+// // import Notification from 'components/Сounter/Statistics/Notification/Notification';
+// import React, { Component } from 'react';
+// // import css from './Counret.module.css';
 
-  countTotalFeedback() {
-    return this.state.good + this.state.neutral + this.state.bad;
-  }
+// class Couter extends Component {
+//   state = {
+//     good: 0,
+//     neutral: 0,
+//     bad: 0,
+//   };
 
-  countPositiveFeedbackPercentage = () => {
-    if (this.countTotalFeedback) {
-      return Math.round((this.state.good / this.countTotalFeedback()) * 100);
-    }
-    return 0;
-  };
+//   countTotalFeedback() {
+//     return this.state.good + this.state.neutral + this.state.bad;
+//   }
 
-  showFeedback = options => {
-    this.setState(prevState => ({ [options]: prevState[options] + 1 }));
-  };
+//   countPositiveFeedbackPercentage = () => {
+//     if (this.countTotalFeedback) {
+//       return Math.round((this.state.good / this.countTotalFeedback()) * 100);
+//     }
+//     return 0;
+//   };
 
-  // handleIncrementGood = () => {
-  //   this.setState(prevState => ({
-  //     good: prevState.good + 1,
-  //   }));
-  // };
+//   showFeedback = options => {
+//     this.setState(prevState => ({ [options]: prevState[options] + 1 }));
+//   };
 
-  // handleIncrementNeutral = () => {
-  //   this.setState(prevState => ({
-  //     neutral: prevState.neutral + 1,
-  //   }));
-  // };
+//   // handleIncrementGood = () => {
+//   //   this.setState(prevState => ({
+//   //     good: prevState.good + 1,
+//   //   }));
+//   // };
 
-  // handleIncrementBad = () => {
-  //   this.setState(prevState => ({
-  //     bad: prevState.bad + 1,
-  //   }));
-  // };
+//   // handleIncrementNeutral = () => {
+//   //   this.setState(prevState => ({
+//   //     neutral: prevState.neutral + 1,
+//   //   }));
+//   // };
 
-  render() {
-    return (
-      <div className={css.counter}>
-        <Section title="Please leave feedback">
-          <Feedback
-            options={Object.keys(this.state)}
-            onLeaveFeedback={this.showFeedback}
-          />
-        </Section>
-        <Section title="Statistics">
-          {this.countTotalFeedback() !== 0 ? (
-            <Statistic
-              good={this.state.good}
-              neutral={this.state.neutral}
-              bad={this.state.bad}
-              total={this.countTotalFeedback()}
-              percentage={this.countPositiveFeedbackPercentage()}
-            />
-          ) : (
-            <Notification message="There is no feedback"></Notification>
-          )}
-        </Section>
-      </div>
-    );
-  }
-}
-export default Couter;
+//   // handleIncrementBad = () => {
+//   //   this.setState(prevState => ({
+//   //     bad: prevState.bad + 1,
+//   //   }));
+//   // };
+
+//   render() {
+//     return (
+//       <div className={css.counter}>
+//         <Section title="Please leave feedback">
+//           <Feedback
+//             options={Object.keys(this.state)}
+//             onLeaveFeedback={this.showFeedback}
+//           />
+//         </Section>
+//         <Section title="Statistics">
+//           {this.countTotalFeedback() !== 0 ? (
+//             <Statistic
+//               good={this.state.good}
+//               neutral={this.state.neutral}
+//               bad={this.state.bad}
+//               total={this.countTotalFeedback()}
+//               percentage={this.countPositiveFeedbackPercentage()}
+//             />
+//           ) : (
+//             <Notification message="There is no feedback"></Notification>
+//           )}
+//         </Section>
+//       </div>
+//     );
+//   }
+// }
+// export default Couter;
